acd2fbe6a8a8d174e1d005431346f6447ace4d10
dineshc01 <dyaniss1994@gmail.com>
2019-08-20 18:22:06 +1000

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,5 +1,5 @@
 class profile::agent_nodes {
   include dockeragent
-  dockeragent::node { 'web.puppetmaster': }
-  dockeragent::node { 'db.puppetmaster': }
+  dockeragent::node { 'web.puppet.vm': }
+  dockeragent::node { 'db.puppet.vm': }
 }
